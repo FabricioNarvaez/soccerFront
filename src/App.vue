9de6aff67b00761d71c1749c189d8ad6e70a8a32
Fabricio_Narvaez <narvaezfabricio38@gmail.com>
2024-02-07 13:54:51 +0100

    [FRONT-40] Create header. WIP: make it responsive

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,38 @@
+<template>
+    <header>
+        <div class="headerContainer">
+            <img alt="AAPD Logo" class="headerLogo"
+                src="https://res.cloudinary.com/dzd68sxue/image/upload/v1697728432/WEBP/Logo_ybmlsh.webp" />
+
+            <!-- <HelloWorld msg="You did it!" /> -->
+            <div class="hamburger" :class="{ change: isActive }" @click="toggleMenu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <nav class="navMenu">
+                <RouterLink to="/">Inicio</RouterLink>
+                <RouterLink to="/Statistics">Estadísticas</RouterLink>
+                <RouterLink to="/classification">Clasificación</RouterLink>
+                <RouterLink to="/matchweeks">Jornadas</RouterLink>
+                <RouterLink to="/admin">
+                    <img alt="Admin icon" class="icon" src="@/assets/images/PhUserCircle.svg" width="40" height="40" />
+                </RouterLink>
+            </nav>
+        </div>
+    </header>
+
+    <RouterView />
+</template>
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 // import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue';
+
+const isActive = ref(false);
+function toggleMenu() {
+    isActive.value = !isActive.value;
+    // emit('toggle');
+}
 </script>
-
-<template>
-  <header>
-    <div class="headerContainer">
-      <img alt="AAPD Logo" class="logo" src="https://res.cloudinary.com/dzd68sxue/image/upload/v1697728432/WEBP/Logo_ybmlsh.webp" width="125" height="125" />
-  
-      <!-- <HelloWorld msg="You did it!" /> -->
-      <nav class="navMenu">
-        <RouterLink to="/">Inicio</RouterLink>
-        <RouterLink to="/Statistics">Estadísticas</RouterLink>
-        <RouterLink to="/classification">Clasificación</RouterLink>
-        <RouterLink to="/matchweeks">Jornadas</RouterLink>
-        <RouterLink to="/admin">
-          <img alt="Admin icon" class="icon" src="@/assets/images/PhUserCircle.svg" width="40" height="40" />
-        </RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
-</template>
-
-
