d3f9bed949bc113be5e2809f84082f95ea327453
Fabricio_Narvaez <narvaezfabricio38@gmail.com>
2024-03-27 17:49:36 +0100

    [FRONT-42] Create css for carousel styles

--- src/components/home/Carousel.vue
+++ src/components/home/Carousel.vue
@@ -14,7 +14,7 @@
 <script setup>
   import { defineComponent } from 'vue'
   import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
-  
+
   import 'vue3-carousel/dist/carousel.css'
   
   defineComponent({
@@ -29,27 +29,7 @@
 </script>
   
   
-<style>
-  .carousel__item {
-    min-height: 200px;
-    width: 100%;
-    /* background-color: var(--vc-clr-primary); */
-    /* color: var(--vc-clr-white); */
-    font-size: 20px;
-    border-radius: 8px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
-  
-  .carousel__slide {
-    padding: 10px;
-  }
-  
-  .carousel__prev,
-  .carousel__next {
-    box-sizing: content-box;
-    border: 5px solid white;
-  }
+<style scoped>
+    @import "src/assets/css/home/carousel.css";
 </style>
   
