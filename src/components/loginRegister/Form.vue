db9cdb92419c1d9dd99e50f68dd8e0447e9cc329
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-04 19:54:54 +0200

    [FRONT-44] Add functionality for loginRegister

--- src/components/loginRegister/Form.vue
+++ src/components/loginRegister/Form.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
         <div class="formGroup">
-            <input v-if="hideRegister" type="input" class="formField" placeholder="Nombre de usuario o Email" name="name" id='name' required />
-            <input v-else type="input" class="formField" placeholder="Nombre" name="name" id='name' required />
+            <input v-if="hideRegister" type="input" class="formField" placeholder="Nombre de usuario o Email" v-model="username" required />
+            <input v-else type="input" class="formField" placeholder="Nombre" v-model="name" required />
             <div class="registerElements" v-if="!hideRegister">
-                <input type="email" class="formField" placeholder="Email" name="email" id='email' required />
-                <input type="number" class="formField" placeholder="Teléfono" name="phoneNumber" id='phoneNumber' required />
+                <input type="email" class="formField" placeholder="Email" v-model="email" required />
+                <input type="input" class="formField" placeholder="Nombre del Equipo" v-model="team" required />
+                <input type="number" class="formField" placeholder="Teléfono" v-model="phoneNumber" required />
             </div>
-            <input type="password" class="formField" placeholder="Contraseña" name="password" id='password' required />
+            <input type="password" class="formField" placeholder="Contraseña" v-model="password" required />
             <button @click="loginRegister">{{ buttonText }}</button>
             <p v-if="hideRegister">¿Quieres participar? <RouterLink class="colorDarkBluePalette boldText" to="/registrar">Crear cuenta</RouterLink></p>
         </div>
@@ -15,7 +16,7 @@
 </template>
 
 <script setup>
-    import { defineProps } from 'vue';
+    import { defineProps, ref } from 'vue';
 
     const props = defineProps({
         buttonText: {
@@ -27,6 +28,52 @@
             required: true
         }
     });
+
+    const username = ref('');
+    const name = ref('');
+    const email = ref('');
+    const team = ref('');
+    const phoneNumber = ref('');
+    const password = ref('');
+
+    const loginRegister = async (hideRegister) => {
+        try {
+            let formData = {};
+            console.log(hideRegister);
+            if (hideRegister) {
+                formData = {
+                    username: username.value,
+                    password: password.value
+                };
+            } else {
+                formData = {
+                    name: name.value,
+                    email: email.value,
+                    team: team.value,
+                    phoneNumber: phoneNumber.value,
+                    password: password.value
+                };
+            }
+            
+            const response = await fetch('URL_DEL_ENDPOINT_DE_LOGIN_O_REGISTRO', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                console.log('Operación exitosa');
+            } else {
+                console.error('Error:', data.message);
+            }
+        } catch (error) {
+            console.error('Error de red:', error);
+        }
+    };
 </script>
 
 <style scoped>
