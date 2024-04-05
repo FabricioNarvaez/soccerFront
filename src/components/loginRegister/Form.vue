da9f0b385edd5613b1b44a90c54a76c9adc2c829
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-05 19:33:17 +0200

    [FRONT-44] Prepare register to connect to API

--- src/components/loginRegister/Form.vue
+++ src/components/loginRegister/Form.vue
@@ -2,18 +2,24 @@
     <div>
         <p v-if="hideRegister">Inicia sesión con tu cuenta de Directivo</p>
         <p v-else>Date de alta como Directivo y crea tu Equipo</p>
-        <div class="formGroup">
-            <input v-if="hideRegister" type="input" class="formField" placeholder="Nombre de usuario o Email" v-model="username" required />
-            <input v-else type="input" class="formField" placeholder="Nombre" v-model="name" required />
+        <form class="formGroup" @submit.prevent="loginRegister" ref="formRef">
+            <input v-if="hideRegister" type="input" class="formField" placeholder="Nombre de usuario o Email" v-model="loginUsername" required />
             <div class="registerElements" v-if="!hideRegister">
-                <input type="email" class="formField" placeholder="Email" v-model="email" required />
-                <input type="input" class="formField" placeholder="Nombre del Equipo" v-model="team" required />
-                <input type="number" class="formField" placeholder="Teléfono" v-model="phoneNumber" required />
+                <input type="input" class="formField" placeholder="Nombre" v-model="name" />
+                <input type="input" class="formField" placeholder="Nombre de usuario *" v-model="username" required />
+                <input type="email" class="formField" placeholder="Email *" v-model="email"  />
+                <input type="input" class="formField" placeholder="Nombre del Equipo *" v-model="team" required />
+                <input type="number" class="formField" placeholder="Teléfono *" v-model="phoneNumber" required />
             </div>
-            <input type="password" class="formField" placeholder="Contraseña" v-model="password" required />
-            <button @click="loginRegister">{{ buttonText }}</button>
+            <input type="password" class="formField" placeholder="Contraseña *" v-model="password" required />
+            <div v-if="!hideRegister" class="checkboxContainer">
+                <input type="checkbox" id="terms-checkbox" class="checkboxInput" v-model="checkboxTerms"/>
+                <label for="terms-checkbox" class="checkboxLabel">He leído y acepto los <RouterLink to="/registrar" class="checkbox-link">términos y condiciones</RouterLink></label>
+            </div>
+            <p v-if="errorTerms" class="errorMessage">Debe aceptar los términos y condiciones para poderse registrar en nuestro sitio web.</p>
+            <button type="submit">{{ buttonText }}</button>
             <p v-if="hideRegister">¿Quieres participar? <RouterLink class="colorDarkBluePalette boldText" to="/registrar">Crear cuenta</RouterLink></p>
-        </div>
+        </form>
     </div>
 </template>
 
@@ -31,27 +37,34 @@
         }
     });
 
+    const loginUsername = ref('');
     const username = ref('');
     const name = ref('');
     const email = ref('');
     const team = ref('');
     const phoneNumber = ref('');
     const password = ref('');
+    const formRef = ref(null);
+    const checkboxTerms = ref(false);
+    const errorTerms = ref(false);
 
     const loginRegister = async () => {
+        errorTerms.value = checkboxTerms.value ? false : true;
+        if(errorTerms.value) return
         try {
             const APIUrl = import.meta.env.VITE_API_URL;
             let formData = {};
             let postUrl = APIUrl;
             if (props.hideRegister) {
                 formData = {
-                    username: username.value,
+                    loginUsername: loginUsername.value,
                     password: password.value
                 };
                 postUrl += "coaches/register";
             } else {
                 formData = {
                     name: name.value,
+                    username: username.value,
                     email: email.value,
                     team: team.value,
                     phoneNumber: phoneNumber.value,
