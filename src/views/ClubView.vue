803502abf5d01e383106d8b8b44b5ecf72c939ac
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-13 12:56:56 +0200

    [FRONT-46] Move banner css to a specific file and create nav

--- src/views/ClubView.vue
+++ src/views/ClubView.vue
@@ -1,14 +1,23 @@
 <template>
-    <div class="clubBanner">
-        <div class="clubBannerLeft">
-            <img src="https://res.cloudinary.com/dzd68sxue/image/upload/v1695395236/WEBP/Cancheritos_qdbnsw.webp"/>
-            <div class="cardTeamInfo">
-                <p><b>Chancheritos</b></p>
-                <p><i>Directivo</i>: Fabricio Narvaez</p>
-                <p><i>Grupo</i>: A</p>
+    <div class="content80">
+        <div class="clubBanner">
+            <div class="clubBannerLeft">
+                <img src="https://res.cloudinary.com/dzd68sxue/image/upload/v1695395236/WEBP/Cancheritos_qdbnsw.webp"/>
+                <div class="cardTeamInfo">
+                    <p><b>Chancheritos</b></p>
+                    <p><i>Directivo</i>: Fabricio Narvaez</p>
+                    <p><i>Grupo</i>: A</p>
+                </div>
             </div>
+            <div class="gradient"></div>
         </div>
-        <div class="gradient"></div>
+    
+        <nav class="clubNav">
+            <RouterLink to="/">General</RouterLink>
+            <RouterLink to="/">Plantilla</RouterLink>
+            <RouterLink to="/">Resultados</RouterLink>
+            <RouterLink to="/">Sanciones</RouterLink>
+        </nav>
     </div>
 </template>
 
@@ -17,5 +26,6 @@
 </script>
 
 <style scoped>
-    @import "../assets/css/club/club.css"
+    @import "../assets/css/club/banner.css";
+    @import "../assets/css/club/club.css";
 </style>
