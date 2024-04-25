dd6141b29419ea59fd5106fe16025bc42df1d3b6
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-25 22:49:28 +0200

    [FRONT-41] Add styles for subtitle

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,11 @@
 <template>
-  <main>
-    <Swiper />
-    <Carousel />
-  </main>
+    <main>
+        <Swiper />
+        <!-- <Carousel /> -->
+    </main>
 </template>
 
 <script setup>
-  import Carousel from '../components/home/Carousel.vue'
-  import Swiper from '../components/home/Swiper.vue'
+    // import Carousel from '../components/home/Carousel.vue'
+    import Swiper from '../components/home/Swiper.vue'
 </script>
