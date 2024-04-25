df106b5f0245bf2bbc80f94f81b8036fcd0d24cb
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-25 22:44:31 +0200

    [FRONT-41] Create swiper component and fix responsive container

---
+++ src/components/home/Swiper.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="headerSubtitle">
+        <h3 class="subtitle">Equipos</h3>
+        <RouterLink to="/equipos">TODOS LOS EQUIPOS</RouterLink>
+    </div>
+    <hr>
+</template>
+
+<script setup></script>
+
+<style setup>
+    @import "../../assets/css/home/swiper.css";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <template>
   <main>
+    <Swiper />
     <Carousel />
   </main>
 </template>
 
 <script setup>
   import Carousel from '../components/home/Carousel.vue'
+  import Swiper from '../components/home/Swiper.vue'
 </script>
