4836607dafd6a2156a4d86ebba16a2cab2d4e6e8
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-26 15:55:45 +0200

    [FRONT-41] Add swiper functionaliti

---
+++ src/components/home/SwiperComponent.vue
@@ -0,0 +1,75 @@
+<template>
+    <swiper v-if="allTeams"
+        :modules="modules"
+        :space-between="50"
+        navigation
+        :pagination="{ clickable: true }"
+        :loop="true"
+        :breakpoints="swiperBreakpoints"
+        >
+        <swiper-slide v-if="teamsToSwiper" v-for="(team, key) in teamsToSwiper" :key="key" >
+            <!-- TODO: add link to club page -->
+            <img :src="team.shield"/> 
+            <p>{{ team.name }}</p>
+        </swiper-slide>
+    </swiper>
+</template>
+
+<script>
+    import { Navigation, Pagination, A11y } from 'swiper/modules';
+    import { Swiper, SwiperSlide } from 'swiper/vue';
+
+    // Import Swiper styles
+    import 'swiper/css';
+    import 'swiper/css/navigation';
+    import 'swiper/css/pagination';
+    import 'swiper/css/scrollbar';
+
+    // Import Swiper styles
+    export default {
+        props: {
+            allTeams: {
+                type: Object
+            }
+        },
+        components: {
+            Swiper,
+            SwiperSlide,
+        },
+        setup(props) {
+            const teamsToSwiper = Object.keys(props.allTeams).length === 2 
+                                    ? props.allTeams.A.concat(props.allTeams.B)
+                                    : props.allTeams;
+
+            const swiperBreakpoints = {
+                320: {       
+                    slidesPerView: 1,
+                    spaceBetween: 10     
+                },          
+                400: {       
+                    slidesPerView: 2,
+                    spaceBetween: 10     
+                },          
+                700: {       
+                    slidesPerView: 3,       
+                    spaceBetween: 50     
+                },   
+            
+                1300: {       
+                    slidesPerView: 5,       
+                    spaceBetween: 30     
+                }
+            };
+
+            return {
+                teamsToSwiper,
+                swiperBreakpoints,
+                modules: [Navigation, Pagination, A11y],
+            };
+        },
+    };
+</script>
+
+<style setup>
+    @import "../../assets/css/home/swiper.css";
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,33 @@
 <template>
     <main>
-        <Swiper />
+        <div class="headerSubtitle">
+            <h3 class="subtitle">Equipos</h3>
+            <RouterLink to="/equipos">TODOS LOS EQUIPOS</RouterLink>
+        </div>
+        <hr>
+        <SwiperComponent v-if="swiperReady" :allTeams="allTeams" />
+        <h3 v-else class="subtitle">Aún no hay equipos en el torneo</h3>
         <!-- <Carousel /> -->
     </main>
 </template>
 
 <script setup>
     // import Carousel from '../components/home/Carousel.vue'
-    import Swiper from '../components/home/Swiper.vue'
+    import SwiperComponent from '../components/home/SwiperComponent.vue';
+    import { ref, onMounted } from "vue";
+    const APIUrl = import.meta.env.VITE_API_URL;
+
+    const swiperReady = ref(false);
+    const allTeams = ref({});
+
+    onMounted(async () => {
+        try {
+            const allTeamsResponse = await fetch(`${APIUrl}/teams/all`);
+            allTeams.value = await allTeamsResponse.json();
+            if(Object.keys(allTeams).length) swiperReady.value = true;
+            console.log(allTeams.value)
+        } catch (error) {
+            console.error('Error al obtener datos:', error);
+        }
+    })
 </script>
