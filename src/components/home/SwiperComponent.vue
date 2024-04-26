f17d2e712f25a92391b38e12a094d6ccf9c8231a
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-04-26 15:57:15 +0200

    Remove coments

--- src/components/home/SwiperComponent.vue
+++ src/components/home/SwiperComponent.vue
@@ -19,13 +19,11 @@
     import { Navigation, Pagination, A11y } from 'swiper/modules';
     import { Swiper, SwiperSlide } from 'swiper/vue';
 
-    // Import Swiper styles
     import 'swiper/css';
     import 'swiper/css/navigation';
     import 'swiper/css/pagination';
     import 'swiper/css/scrollbar';
 
-    // Import Swiper styles
     export default {
         props: {
             allTeams: {
