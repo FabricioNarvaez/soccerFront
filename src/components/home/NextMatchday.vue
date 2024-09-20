c7b46e959a0d42dd291d87406b6b13f5bf488991
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-09-20 12:58:10 +0200

    Refactor creating NextMatchday component

---
+++ src/components/home/NextMatchday.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="leftMain">
+        <HeaderSubtitle :subtitle="'Próxima Jornada'" :link="'/'" :linkText="'TODAS LAS JORNADAS'" />
+        <div class="matchOfMatchweekContainer" v-if="upcomingMatchweek" v-for="(match, key) in upcomingMatchweek.matches" :key="key">
+            <div class="matchTeams">
+                <div class="localInfo">
+                    <img class="matchTeamsImg" :src="match.localId.shield" />
+                    <p>{{ match.localId.name }}</p>
+                </div>
+                <p class="versus">VS</p>
+                <div class="visitorInfo">
+                    <img class="matchTeamsImg" :src="match.visitorId.shield" />
+                    <p>{{ match.visitorId.name }}</p>
+                </div>
+            </div>
+            <div class="matchHour">{{ match.formattedDate }} | {{ match.formattedHour }} </div>
+        </div>
+        <p v-else class="subtitle">Las jornadas aún no están definidas</p>
+    </div>
+</template>
+
+<script setup>
+    import HeaderSubtitle from '../common/HeaderSubtitle.vue';
+
+    defineProps({
+        upcomingMatchweek: {
+            type: Object,
+            required: true
+        }
+    })
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,24 +5,7 @@
         <p v-else class="subtitle">Aún no hay equipos en el torneo</p>
         <!-- <Carousel /> -->
         <div class="homeMain">
-            <div class="leftMain">
-                <HeaderSubtitle :subtitle="'Próxima Jornada'" :link="'/'" :linkText="'TODAS LAS JORNADAS'" />
-                <div class="matchOfMatchweekContainer" v-if="upcomingMatchweek" v-for="(match, key) in upcomingMatchweek.matches" :key="key">
-                    <div class="matchTeams">
-                        <div class="localInfo">
-                            <img class="matchTeamsImg" :src="match.localId.shield" />
-                            <p>{{ match.localId.name }}</p>
-                        </div>
-                        <p class="versus">VS</p>
-                        <div class="visitorInfo">
-                            <img class="matchTeamsImg" :src="match.visitorId.shield" />
-                            <p>{{ match.visitorId.name }}</p>
-                        </div>
-                    </div>
-                    <div class="matchHour">{{ match.formattedDate }} | {{ match.formattedHour }} </div>
-                </div>
-                <p v-else class="subtitle">Las jornadas aún no están definidas</p>
-            </div>
+            <NextMatchday :upcomingMatchweek="upcomingMatchweek"/>
             <div class="rightMain">
                 <p v-if="classifications">tabla clasificaciones</p>
                 <p v-else>Las tablas de clasificaciones aún no están definidas.</p>
@@ -35,6 +18,7 @@
     // import Carousel from '../components/home/Carousel.vue'
     import SwiperComponent from '../components/home/SwiperComponent.vue';
     import HeaderSubtitle from '../components/common/HeaderSubtitle.vue';
+    import NextMatchday from '../components/home/NextMatchday.vue';
 
     import { ref, onMounted } from "vue";
     const APIUrl = import.meta.env.VITE_API_URL;
