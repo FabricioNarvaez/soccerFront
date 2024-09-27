a45a6040436b209425b0f4a39aa58e4ac47058f0
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-09-27 12:15:35 +0200

    [FRONT-49] Add subtitle to SmallTable component

---
+++ src/components/home/SmallTable.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="rightMain">
+        <HeaderSubtitle :subtitle="'Tabla Clasificaciones'" :link="'/'" :linkText="'Mostrar tablas completas'" />
+        <p v-if="groupA || groupB">tabla clasificaciones</p>
+        <p v-else>Las tablas de clasificaciones aún no están definidas.</p>
+    </div>
+</template>
+
+<script setup>
+    import { ref, onMounted } from "vue";
+    import HeaderSubtitle from '../common/HeaderSubtitle.vue';
+
+    defineProps({
+        groupA: {
+            type: Array,
+            required: true
+        },
+        groupB: {
+            type: Array,
+            required: true
+        },
+    })
+</script>
