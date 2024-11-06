3eab2c7a65971c44dd2e276ea1c548187fac3e56
FabricioNarvaez <narvaezfabricio38@gmail.com>
2024-11-06 19:49:52 +0100

    [FRONT-51] Add table to classifications view

---
+++ src/components/common/BigTable.vue
@@ -0,0 +1,51 @@
+<template>
+    <div>
+        <h3 class="subtitle">Grupo {{ groupName }}</h3>
+        <table>
+                <thead>
+                    <tr>
+                        <th>POS</th>
+                        <th>EQUIPO</th>
+                        <th>PJ</th>
+                        <th>PG</th>
+                        <th>PE</th>
+                        <th>PP</th>
+                        <th>GF</th>
+                        <th>GC</th>
+                        <th>DG</th>
+                        <th>PTS</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in groupData" :key="groupData">
+                        <td>{{ index + 1}}</td>
+                        <td class="teamNameWithShield">
+                            <img :src=item.shield />
+                            {{ item.name }}
+                        </td>
+                        <td>{{ item.PE + item.PP + item.PG  }}</td>
+                        <td>{{ item.PG }}</td>
+                        <td>{{ item.PE }}</td>
+                        <td>{{ item.PP }}</td>
+                        <td>{{ item.GF }}</td>
+                        <td>{{ item.GC }}</td>
+                        <td>{{ item.GD }}</td>
+                        <td>{{ item.Pts }}</td>
+                    </tr>
+                </tbody>
+            </table>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        groupName: {
+            type: String,
+            required: true
+        },
+        groupData: {
+            type: Array,
+            required: true
+        }
+    });
+</script>
