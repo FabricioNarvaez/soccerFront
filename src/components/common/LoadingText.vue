fa39e3c32fc699235b02e203ffe1e522f00a612e
FabricioNarvaez <narvaezfabricio38@gmail.com>
2025-11-05 12:12:26 +0100

    Delete swiper.css file and apply tailwind styles on swiper component. Create LoadingText component

---
+++ src/components/common/LoadingText.vue
@@ -0,0 +1,14 @@
+<template>
+    <p class="text-xl text-gray-500 text-center font-medium">
+        {{ loadingText }}
+    </p>
+</template>
+
+<script setup>
+    defineProps({
+        loadingText: {
+            type: String,
+            default: 'Cargando...'
+        }
+    });
+</script>
--- src/components/home/SwiperComponent.vue
+++ src/components/home/SwiperComponent.vue
@@ -1,32 +1,47 @@
 <template>
-    <!-- TODO: Cambiar por un loader -->
-    <p v-if="teamsStore.loading" class="subtitle">Aún no hay equipos en el torneo</p>
-    <swiper v-else
-        :modules="modules"
-        :space-between="50"
-        navigation
-        :pagination="{ clickable: true }"
-        :loop="true"
-        :breakpoints="swiperBreakpoints"
-        >
-        <swiper-slide v-if="teamsStore.allTeams" v-for="(team, key) in teamsStore.allTeams" :key="key" >
-            <RouterLink :to="{ path: '/equipo/General', query: { id: team._id, name: team.name }}" >
-                <img :src="team.shield"/> 
-                <p>{{ team.name }}</p>
-            </RouterLink>
-        </swiper-slide>
-        <p v-else class="subtitle">Aún no hay equipos en el torneo</p>
-    </swiper>
+    <div class="py-8">
+        <LoadingText v-if="teamsStore.loading" :loadingText="'Cargando Equipos...'"/>
+
+        <p v-else-if="!teamsStore.allTeams || teamsStore.allTeams.length === 0" class="text-xl text-gray-500 text-center font-medium">
+            Aún no hay equipos en el torneo
+        </p>
+
+        <swiper v-else class="pb-10" 
+            :modules="modules"
+            :space-between="50"
+            navigation
+            :pagination="{ clickable: true }"
+            :loop="true"
+            :breakpoints="swiperBreakpoints"
+            >
+            
+            <swiper-slide v-for="(team, key) in teamsStore.allTeams" :key="key" class="!pb-8">
+                <RouterLink class="flex flex-col items-center space-y-3 p-4 transition-transform duration-300 hover:scale-[1.02]"
+                    :to="{ path: '/equipo/General', query: { id: team._id, name: team.name }}"
+                >
+                    <img class="w-full max-w-[200px] h-auto object-contain sm:max-w-[160px] xl:max-w-[200px]"
+                        :src="team.shield" 
+                        :alt="`Escudo del equipo ${team.name}`"
+                    />
+                    
+                    <p class="text-lg font-bold text-[#206192] mt-2 text-center truncate w-full px-2">
+                        {{ team.name }}
+                    </p>
+                </RouterLink>
+            </swiper-slide>
+        </swiper>
+    </div>
 </template>
 
 <script setup>
     import { Navigation, Pagination, A11y } from "swiper/modules";
     import { Swiper, SwiperSlide } from "swiper/vue";
+    import LoadingText from "@components/common/LoadingText.vue";
 
     import "swiper/css";
     import "swiper/css/navigation";
     import "swiper/css/pagination";
-    import "swiper/css/scrollbar";
+    // import "swiper/css/scrollbar";
 
     import { useTeamsStore } from '@store/teamsStore.js';
     const teamsStore = useTeamsStore();
@@ -40,8 +55,3 @@
 
     const modules = [Navigation, Pagination, A11y];
 </script>
-
-
-<style setup>
-    @import "@css/home/swiper.css";
-</style>
